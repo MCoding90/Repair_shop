be996ce07d3280211f481cd514abe1d3f509afa0
czerw <czerwonkaster@gmail.com>
2021-12-08 11:50:33 +0000

    Initial commit

---
+++ solent_repairs.py
@@ -0,0 +1,43 @@
+#Solent Computer Repairs
+
+print("***********************")
+print("Solent Computer Repairs")
+print("***********************")
+
+print("What would you like to do?:")
+option = input()
+print("[1] Book item")
+print("[2] search for a specific item by the customer")
+print("[3] display all items booked for repair, sorted by customer name")
+print("[4] search for an item by customer name")
+print("[5] delete a specific item")
+print("[6] answer enquiries")
+print("[7] exit")
+
+
+
+# Create 3 lists to store data inputted by shop staff
+customers = []
+items = []
+fault= []
+
+def booking():
+
+    customer_name = input("Customer name: ")
+    item = input("item: ")
+    fault_description = input("fault description: ")
+    return
+
+
+
+#Display all items booked for repair
+
+#search for a specific item sent in for repair by the customer
+
+#display all items booked for repair, sorted by customer name
+
+#search for an item by customer name
+
+#The member of staff must be able to delete a specific item
+
+#“answer enquiries” functionality
