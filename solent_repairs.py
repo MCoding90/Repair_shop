29a2b9fbd3bc0fdb9536262bd3b5c6f53a426cb6
czerw <czerwonkaster@gmail.com>
2021-12-15 21:42:01 +0000

    option 6 works

--- solent_repairs.py
+++ solent_repairs.py
@@ -15,46 +15,69 @@ def show_menu():
     print("[7] exit")
 
 
+def booking():
+    print("Please enter Customer details!")
+    customer_name = input("Customer name: ")
+    item = input("Item: ")
+    fault_description = input("Fault description: ")
+
+    customer_list = [customer_name, item, fault_description]
+    customers.append(customer_list)
+
+    print(customers)
+
+
 loop = True
 
 while loop:
     customers = []
 
     show_menu()
-    option = input("What would you like to do: ?")
+    option = input("\nWhat would you like to do: ?")
 
     if option == '1':
-        print("option 1 has been selected")
-
-        print("Please enter Customer details!")
-        customer_name = input("Customer name: ")
-        item = input("Item: ")
-        fault_description = input("Fault description: ")
-
-        customers.append(customer_name)
-        customers.append(item)
-        customers.append(fault_description)
-
-        print(customers)
+        print("\noption 1 has been selected")
+        booking()
+        print("\nWould you like to book another?:")
 
     elif option == '2':
-        print("option 2 has been selected")
-
-
+        print("\noption 2 has been selected")
+        print(customers)
     elif option == '3':
-        print("option 3 has been selected")
+        print("\noption 3 has been selected")
 
     elif option == '4':
-        print("option 4 has been selected")
+        print("\noption 4 has been selected")
 
     elif option == '5':
-        print("option 5 has been selected")
+        print("\noption 5 has been selected")
+
+        print("\nEnter id to delete a booking")
+        input1 = input()
+        for i in customers[:]:
+            if i[3] == input1: 2
+            customers.remove(i)
+            print(customers[:])
 
     elif option == '6':
-        print("option 5 has been selected")
+        print("\noption 6 has been selected")
+        queue = ['My computer is running slow – what can I do about it?',
+                 'Which web browser would you recommend, and why?',
+                 'Can you recommend the PC build for gamer?',
+                 'Why there is no audio output?']
+
+        # Removing elements from the queue
+        print("\nEnquiries answered")
+        print(queue.pop(0))
+        print(queue.pop(0))
+        print(queue.pop(0))
+        print(queue.pop(0))
+
+        print("\nAll enquiries answered")
+        print(queue)
 
     elif option == '7':
-        print("option 7 has been selected")
+        print("\noption 7 has been selected")
         loop = False  # This will make the while loop to end as not value of loop is set to False
     else:
-        print("Wrong option selected! Please choose a valid option!")
+        print("\nWrong option selected! Please choose a valid option!")
